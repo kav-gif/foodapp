c4a4cbfbc8435d5d5ee17c01aadb18698d084f52
kavinda <kavindadilhara129@gmail.com>
2022-03-19 00:48:46 +0530

    Final commit

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_f&_c+r9bs=lyyojngu7$2vu-g!!&p7+l+28_*+4oox%pqwn^3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['hotpotfamilyrestaurant.herokuapp.com']
+ALLOWED_HOSTS = [ '127.0.0.1','hotpotfamilyrestaurant.herokuapp.com']
 
 
 # Application definition
