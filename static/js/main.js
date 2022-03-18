e9b501e2b694af117c5807edf7872e30b4634e34
kavinda <kavindadilhara129@gmail.com>
2022-03-18 23:14:10 +0530

    fourth commit

--- static/js/main.js
+++ static/js/main.js
@@ -4,34 +4,41 @@ const noodles = document.querySelector('.noodles');
 const rice_menu = document.querySelector('.rice_menu');
 const soup_menu = document.querySelector('.soup_menu');
 const noodles_menu = document.querySelector('.noodles_menu');
+const details = document.querySelector('.details');
 
 
 rice.addEventListener('click',()=>{
     if(rice_menu.className !== 'rice_menu height'){
         rice_menu.classList.add('height')
+        details.classList.add('hide')
         soup_menu.classList.remove('height')
         noodles_menu.classList.remove('height')
     }else{
         rice_menu.classList.remove('height')
+        details.classList.remove('hide')
     }    
 })
 
 soup.addEventListener('click',()=>{
     if(soup_menu.className !== 'soup_menu height'){
         soup_menu.classList.add('height')
+        details.classList.add('hide')
         rice_menu.classList.remove('height')
         noodles_menu.classList.remove('height')
     }else{
         soup_menu.classList.remove('height')
+        details.classList.remove('hide')
     }    
 })
 
 noodles.addEventListener('click',()=>{
     if(noodles_menu.className !== 'noodles_menu height'){
         noodles_menu.classList.add('height')
+        details.classList.add('hide')
         rice_menu.classList.remove('height')
         soup_menu.classList.remove('height')
     }else{
         noodles_menu.classList.remove('height')
+        details.classList.remove('hide')
     }    
 })
